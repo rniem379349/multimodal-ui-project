974568e19266c1d952233d28751f29a4a05a3581
Gabriela Dinica <dinica.gabriela@gmail.com>
2019-04-09 20:30:12 +0200

    Repeated Listening + Initial logging
    List of changes:
    - Adjusting microphone for ambient noise at beginning of run
    - Listen to user input in a repeate infinite loop
    - Added a debuggin command.
    - Added debug logging to a default file

--- speech.py
+++ speech.py
@@ -2,12 +2,23 @@
 
 import speech_recognition as sr
 import pyautogui as pag
+import datetime
 
+now = datetime.datetime.now()
+
+def write_to_file(message):
+    output = now.isoformat() + ": " + message + '\n'
+    f = open('debug.txt', 'a')
+    f.write(output)
+    f.close()
 
 def execute_voice_cmd(command):
     command_matcher = COMMANDS
     func = command_matcher.get(command, lambda: "Invalid command")
-    print(func())
+    result = func()
+    if result is None:
+        write_to_file(command)
+    print(result)
 
 
 # functions for all the voice commands
@@ -38,6 +49,9 @@ def switch_source_one_two():
 def switch_source_two_one():
     pag.hotkey('alt','4')
     
+def audio_test():
+    pag.typewrite('DEBUG OK')
+    
 
 
 # dictionary with the commands and their corresponding functions
@@ -50,32 +64,40 @@ COMMANDS = {
     'switch to source two': switch_source_two,
     'switch to source 1 and 2': switch_source_one_two,
     'switch to source 2 and 1': switch_source_two_one,
-    'switch source': switch_source
+    'switch source': switch_source,
+    'audio test': audio_test
 }
 
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+m = sr.Microphone()
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
+# First adjust for ambient noise before we listen.
+with m as source:
+    r.adjust_for_ambient_noise(source)
+	
+while True:
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-    # get lowercase voice command token
-    voice_cmd = r.recognize_google(audio).lower()
-    print("Your command: " + voice_cmd)
+        # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
 
-    # try to execute the command
-    execute_voice_cmd(voice_cmd)
+        # get lowercase voice command token
+        voice_cmd = r.recognize_google(audio).lower()
+        print("Your command: " + voice_cmd)
 
+        # try to execute the command
+        execute_voice_cmd(voice_cmd)
 
-# exception handling
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # exception handling
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        write_to_file('Error in recognition')
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
